d9c9affe6592b1212dd4b3e144720b9f538396ee
Alwaly <ndiayealwaly999@gmail.com>
2023-09-19 19:33:13 +0000

    Fonction qui permet de créer un fichier

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ import subprocess
 
 def create_folder(name: str):
     os.mkdir(name)
+
+def create_file(name: str, content=""):
+    fd = os.open(name, os.O_RDWR|os.O_CREAT)
+    os.write(fd, content.encode())
+
+def create_first_rank_file():
+     for file in files:
+        create_file(file)
+
