1ae10f7dcc72cbfb44d4e02f57fe6827778a9ee7
Alwaly <ndiayealwaly999@gmail.com>
2023-09-19 19:35:32 +0000

    Fonction qui permet de regrouper l'ensemble des fonctions précédentes

--- app.py
+++ app.py
@@ -14,3 +14,16 @@ def create_first_rank_file():
      for file in files:
         create_file(file)
 
+def create_first_rank_folder_subfolders_and_subfiles():
+     for i in range(len(folders_names)):
+        create_folder(folders_names[i]['folder']["nom"])
+        try:
+            for k in folders_names[i]['folder']["subfolder"]:
+                create_folder(os.path.join(folders_names[i]['folder']["nom"], k))
+        except:
+            try:
+                for file in folders_names[i]['folder']["file"]:
+                    create_file(os.path.join(folders_names[i]['folder']["nom"], file))
+            except:
+                continue
+
